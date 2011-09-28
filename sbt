f52e90c03253ffd4a2aefe501ab1ef6d65ee6096
Alex Bleasdale <alex.bleasdale@marklogic.com>
2011-09-28 17:45:03 +0100

    Added one example test for sbt

--- sbt
+++ sbt
@@ -1,1 +1,1 @@
-sudo java -Xmx512M -jar bin/sbt-launch.jar "$@"
+java -Xmx512M -jar bin/sbt-launch.jar "$@"
---
+++ src/test/scala/Placeholder.scala
@@ -0,0 +1,12 @@
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+
+class Placeholder extends Spec with ShouldMatchers {
+
+
+  describe("Demo") {
+    it("should run") {
+      1 + 1 should be(2)
+    }
+  }
+}
